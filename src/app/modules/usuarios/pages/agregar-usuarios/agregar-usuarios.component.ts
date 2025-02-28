0db6739d110b8b6005bdcd9b40311fa245368b0d
unknown <tu@example.com>
2025-02-28 09:53:40 -0500

    add

--- src/app/modules/usuarios/pages/agregar-usuarios/agregar-usuarios.component.ts
+++ src/app/modules/usuarios/pages/agregar-usuarios/agregar-usuarios.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-usuarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './agregar-usuarios.component.html',
   styleUrl: './agregar-usuarios.component.css'
 })
 export class AgregarUsuariosComponent {
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
 
+  @Output() usuarioAgregado = new EventEmitter<any>();
+
+  agregarUsuario() {
+    if (this.nombre.trim() && this.email.trim() && this.telefono.trim()) {
+      const nuevoUsuario = {
+        name: this.nombre,
+        email: this.email,
+        phone: this.telefono
+      };
+
+      this.usuarioAgregado.emit(nuevoUsuario);
+
+      // Limpiar campos después de agregar
+      this.nombre = '';
+      this.email = '';
+      this.telefono = '';
+    } else {
+      alert('Todos los campos son obligatorios');
+    }
+  }
 }
--- src/app/modules/usuarios/pages/listado-usuarios/listado-usuarios.component.ts
+++ src/app/modules/usuarios/pages/listado-usuarios/listado-usuarios.component.ts
@@ -1,18 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { UserService } from '../../service/usuarios.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgregarUsuariosComponent } from '../agregar-usuarios/agregar-usuarios.component';
 
 @Component({
   selector: 'app-listado-usuarios',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, AgregarUsuariosComponent],
   templateUrl: './listado-usuarios.component.html',
   styleUrl: './listado-usuarios.component.css'
 })
 export class ListadoUsuariosComponent {
 
   usuarios: any[] = [];
+  eliminarUsuario(index: number) {
+    this.usuarios.splice(index, 1);
+  }
 
+  editarUsuario(user: any) {
+    alert(`Editar usuario: ${user.name}`);
+  }
   constructor(private userService: UserService) { }
 
   ngOnInit() {
@@ -29,6 +37,11 @@ export class ListadoUsuariosComponent {
         console.error('Error al cargar usuarios:', err);
       }
     });
+  }
+
+
+  agregarUsuario(usuario: any) {
+    this.usuarios.push(usuario);
   }
 
 }
